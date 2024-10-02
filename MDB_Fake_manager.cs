82ee1d0173e9e2fc3edbbb8d244edf169763fdac
jaco <ventas@jacoagency.io>
2024-10-02 16:29:10 -0700

    mongi

--- MDB_Fake_manager.cs
+++ MDB_Fake_manager.cs
@@ -1,138 +1,109 @@
-// this is a fake mongoDB
-// it contains fake functions for WRITE (PUSH) data into mongoDB and also GET data from mongoDB
-
-using System;
 using System.Collections;
-using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using UnityEngine;
 
 public class MDB_PlayerData : MonoBehaviour
 {
-	//===============================================================
-	public static MDB_PlayerData Instance;
-	//===============================================================
+    public static MDB_PlayerData Instance;
+    private static string connectionString = "mongodb+srv://root:root@cluster0.qbkfnji.mongodb.net/unity?retryWrites=true&w=majority&appName=Cluster0";
+    private float _fakeResponseTime = 0.5f;
 
-	// same data structure as in local player data (top10 scoreboard is handled in other script)
-	[HideInInspector] public static string MDB_WalletAddress;
-	[HideInInspector] public static string MDB_GamerTag;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
-	[HideInInspector] public static int MDB_LastScore;
-	[HideInInspector] public static int MDB_HighScore;
+    public IEnumerator MDB_PUSH_username()
+    {
+        yield return new WaitForSeconds(_fakeResponseTime);
+        
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("GameDatabase");
+        var collection = database.GetCollection<BsonDocument>("PlayerData");
 
-	[HideInInspector] public static int MDB_TimesPlayed;
-	[HideInInspector] public static string MDB_LastPlayedDateString;
-	[HideInInspector] public static string MDB_LastPlayedTxidString;
+        var update = Builders<BsonDocument>.Update.Set("GamerTag", LocalPlayerData.GamerTag);
+        await collection.UpdateOneAsync(
+            Builders<BsonDocument>.Filter.Eq("WalletAddress", LocalPlayerData.WalletAddress),
+            update
+        );
+    }
 
-	//===============================================================
-	private float _fakeResponseTime = 0.5f;
+    public IEnumerator MDB_PUSH_registerLastPlayed()
+    {
+        yield return new WaitForSeconds(_fakeResponseTime);
 
-	//===============================================================
-	private void Awake()
-	{
-		if (Instance != null && Instance != this)
-		{
-			Destroy(gameObject);
-			return;
-		}
-		else if (Instance == null)
-		{
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
-	}
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("GameDatabase");
+        var collection = database.GetCollection<BsonDocument>("PlayerData");
 
-	// si no existe el usuario con la WalletAddress que paga para jugar, es porque es necesario crear un nuevo usuario en mongoDB
-	public void SetupDefaultNewDB()
-	{
-		MDB_WalletAddress = "aSolWalletAddress";
-		MDB_GamerTag = "gamerTag";
+        var update = Builders<BsonDocument>.Update
+            .Set("TimesPlayed", LocalPlayerData.TimesPlayed)
+            .Set("LastPlayedDateString", LocalPlayerData.LastPlayedDateString)
+            .Set("LastPlayedTxidString", LocalPlayerData.LastPlayedTxidString);
 
-		MDB_LastScore = 0;
-		MDB_HighScore = 0;
+        await collection.UpdateOneAsync(
+            Builders<BsonDocument>.Filter.Eq("WalletAddress", LocalPlayerData.WalletAddress),
+            update
+        );
+    }
 
-		MDB_TimesPlayed = 0;
-		MDB_LastPlayedDateString = "date";
-		MDB_LastPlayedTxidString = "txid";
-	}
+    public IEnumerator MDB_PUSH_LastScore()
+    {
+        yield return new WaitForSeconds(_fakeResponseTime);
 
-	// base de datos falsa para prueba
-	private void SetupFakeDB()
-	{
-		MDB_WalletAddress = "aSolWalletAddress";
-		MDB_GamerTag = "Billy";
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("GameDatabase");
+        var collection = database.GetCollection<BsonDocument>("PlayerData");
 
-		MDB_LastScore = 13;
-		MDB_HighScore = 34;
+        var update = Builders<BsonDocument>.Update.Set("LastScore", LocalPlayerData.LastScore);
+        await collection.UpdateOneAsync(
+            Builders<BsonDocument>.Filter.Eq("WalletAddress", LocalPlayerData.WalletAddress),
+            update
+        );
+    }
 
-		MDB_TimesPlayed = 4;
-		MDB_LastPlayedDateString = "date";
-		MDB_LastPlayedTxidString = "txid";
-	}
+    public IEnumerator MDB_PUSH_HighScore()
+    {
+        yield return new WaitForSeconds(_fakeResponseTime);
 
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("GameDatabase");
+        var collection = database.GetCollection<BsonDocument>("PlayerData");
 
+        var update = Builders<BsonDocument>.Update.Set("HighScore", LocalPlayerData.HighScore);
+        await collection.UpdateOneAsync(
+            Builders<BsonDocument>.Filter.Eq("WalletAddress", LocalPlayerData.WalletAddress),
+            update
+        );
+    }
 
-	//=============================================================== PUSH / SETTERS
+    public static int MDB_GET_LastScore()
+    {
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("GameDatabase");
+        var collection = database.GetCollection<BsonDocument>("PlayerData");
 
-	// push new username (GamerTag) se sobreescribe cada vez que se abre el link del juego
-	public IEnumerator MDB_PUSH_username()
-	{
-		yield return new WaitForSeconds(_fakeResponseTime);
-		MDB_GamerTag = LocalPlayerData.GamerTag;
-	}
+        var document = collection.Find(Builders<BsonDocument>.Filter.Eq("WalletAddress", LocalPlayerData.WalletAddress)).FirstOrDefault();
+        return document != null ? document["LastScore"].AsInt32 : 0;
+    }
 
-	// cada vez que se paga se registra
-	public IEnumerator MDB_PUSH_registerLastPlayed()
-	{
-		yield return new WaitForSeconds(_fakeResponseTime);
-		MDB_TimesPlayed = LocalPlayerData.TimesPlayed;
-		MDB_LastPlayedDateString = LocalPlayerData.LastPlayedDateString;
-		MDB_LastPlayedTxidString = LocalPlayerData.LastPlayedTxidString;
-	}
+    public static int MDB_GET_HighScore()
+    {
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("GameDatabase");
+        var collection = database.GetCollection<BsonDocument>("PlayerData");
 
-	// lo utiliza el LocalPlayerData cada que el usuario pierde 1 vida
-	public IEnumerator MDB_PUSH_LastScore()
-	{
-		yield return new WaitForSeconds(_fakeResponseTime);
-		MDB_LastScore = LocalPlayerData.LastScore;
-	}
-
-	// lo utiliza el LocalPlayerData cada que el usuario pierde 1 vida (solamente si hay un nuevo highscore)
-	public IEnumerator MDB_PUSH_HighScore()
-	{
-		yield return new WaitForSeconds(_fakeResponseTime);
-		MDB_HighScore = LocalPlayerData.HighScore;
-	}
-
-
-	// estas funciones son falsas! simplemente regresan el valor que esta escrito en este mismo script de GamerTag, LastScore... etc ....
-	//=============================================================== GET
-	public static string MDB_GET_Gamertag()
-	{
-		return MDB_GamerTag;
-	}
-
-	public static int MDB_GET_LastScore()
-	{
-		return MDB_LastScore;
-	}
-
-	public static int MDB_GET_HighScore()
-	{
-		return MDB_HighScore;
-	}
-
-	public static int MDB_GET_TimesPlayed()
-	{
-		return MDB_TimesPlayed; 
-	}
-
-	public static string MDB_GET_LastPlayedDate()
-	{
-		return MDB_LastPlayedDateString;
-	}
-
-	public static string MDB_GET_LastPlayedTxid()
-	{
-		return MDB_LastPlayedTxidString;
-	}
+        var document = collection.Find(Builders<BsonDocument>.Filter.Eq("WalletAddress", LocalPlayerData.WalletAddress)).FirstOrDefault();
+        return document != null ? document["HighScore"].AsInt32 : 0;
+    }
 }
--- MDB_Scoreboard.cs
+++ MDB_Scoreboard.cs
@@ -1,99 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using UnityEngine;
-
-// fake mongoDB scoreboard
-// this script handles the Top10 Scoreboard data 
 
 public class MDB_Scoreboard : MonoBehaviour
 {
-	//===============================================================
-	public static MDB_Scoreboard Instance;
+    public static MDB_Scoreboard Instance;
+    private static string connectionString = "mongodb+srv://root:root@cluster0.qbkfnji.mongodb.net/unity?retryWrites=true&w=majority&appName=Cluster0";
+    private float _fakeResponseTime = 0.5f;
 
-	private static System.Random random = new System.Random();
-	//===============================================================
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
 
-	// este es la lista de top10... son pares de String con Highscore (int)
-	[HideInInspector] public List<KeyValuePair<string, int>> MDB_Top10Scoreboard;
+        SetupRandomTop10_list();
+    }
 
-	//===============================================================
-	private float _fakeResponseTime = 0.5f;
+    public List<KeyValuePair<string, int>> MDB_GET_Scoreboard()
+    {
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("GameDatabase");
+        var collection = database.GetCollection<BsonDocument>("Scoreboard");
 
-	//===============================================================
-	private void Awake()
-	{
-		if (Instance != null && Instance != this)
-		{
-			Destroy(gameObject);
-			return;
-		}
-		else if (Instance == null)
-		{
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
+        var scoreboard = collection.Find(new BsonDocument()).ToList();
+        List<KeyValuePair<string, int>> top10List = new List<KeyValuePair<string, int>>();
 
-		SetupRandomTop10_list();
-	}
+        foreach (var item in scoreboard)
+        {
+            string gamerTag = item["GamerTag"].AsString;
+            int highScore = item["HighScore"].AsInt32;
+            top10List.Add(new KeyValuePair<string, int>(gamerTag, highScore));
+        }
 
-	//===============================================================================================================
-	// simplemente regresa la lista que esta guardada en este mismo scrip (top10 fake)
-	public List<KeyValuePair<string, int>> MDB_GET_Scoreboard()
-	{
-		return MDB_Top10Scoreboard;
-	}
+        return top10List.OrderByDescending(x => x.Value).ToList();
+    }
 
-	//===============================================================================================================
-	// PUSH NEW top10 LIST TO MONGODB
-	// sobreescribe la lista de este mismo script... deberia sobreescribir la lista de top10 de mongo
-	public IEnumerator MDB_PUSH_newTop10scoreboard_list_co()
-	{
-		yield return new WaitForSeconds(_fakeResponseTime);
+    public IEnumerator MDB_PUSH_newTop10scoreboard_list_co()
+    {
+        yield return new WaitForSeconds(_fakeResponseTime);
 
-		MDB_Top10Scoreboard = LocalPlayerData.Instance.Local_Top10Scoreboard;
-	}
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("GameDatabase");
+        var collection = database.GetCollection<BsonDocument>("Scoreboard");
 
-	//===============================================================================================================
-	// setup a fake top10 default list
-	// es una funcion para hacer un top10 falso
-	private void SetupRandomTop10_list()
-	{
-		MDB_Top10Scoreboard = PopulateList();
-	}
+        // Remove all existing top10 records (optional based on your design)
+        await collection.DeleteManyAsync(new BsonDocument());
 
-	//===============================================================================================================
-	// generates a list of 10 items with a random string and value
-	// it's random GamerTag string and random HighScore value for each one
-	public static List<KeyValuePair<string, int>> PopulateList()
-	{
-		List<KeyValuePair<string, int>> list = new List<KeyValuePair<string, int>>();
+        foreach (var kvp in LocalPlayerData.Instance.Local_Top10Scoreboard)
+        {
+            var document = new BsonDocument
+            {
+                { "GamerTag", kvp.Key },
+                { "HighScore", kvp.Value }
+            };
 
-		for (int i = 0; i < 10; i++)
-		{
-			string key = GenerateRandomKey();
-			int value = random.Next(0, 21); // Upper bound is exclusive
+            await collection.InsertOneAsync(document);
+        }
+    }
 
-			// Add a new KeyValuePair to the list
-			list.Add(new KeyValuePair<string, int>(key, value));
-		}
+    private void SetupRandomTop10_list()
+    {
+        MDB_Top10Scoreboard = PopulateList();
+    }
 
-		return list;
-	}
+    public static List<KeyValuePair<string, int>> PopulateList()
+    {
+        List<KeyValuePair<string, int>> list = new List<KeyValuePair<string, int>>();
+        for (int i = 0; i < 10; i++)
+        {
+            string key = GenerateRandomKey();
+            int value = new System.Random().Next(0, 21); // Upper bound is exclusive
+            list.Add(new KeyValuePair<string, int>(key, value));
+        }
+        return list;
+    }
 
-	//===============================================================================================================
-	// generates a string of 4 random letters
-	private static string GenerateRandomKey()
-	{
-		const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		int number = 12;
-		char[] key = new char[number];
-
-		for (int i = 0; i < number; i++)
-		{
-			key[i] = chars[random.Next(chars.Length)];
-		}
-
-		return new string(key);
-	}
-	//===============================================================================================================
+    private static string GenerateRandomKey()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return new string(Enumerable.Repeat(chars, 12)
+          .Select(s => s[new System.Random().Next(s.Length)]).ToArray());
+    }
 }
